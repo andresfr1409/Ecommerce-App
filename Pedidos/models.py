6a28f277f7c7a57566ba1f98a97c9d0237291750
Andres.14 <feliperin14@hotmail.es>
2024-01-16 19:31:40 -0500

    creacion de modelos para gestionar los pedidos de el usuarioen la aplicacion Pedidos

--- Pedidos/models.py
+++ Pedidos/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from Tienda.models import Producto
+class Pedido(models.Model):
+    user_id = models.ForeignKey(User, on_delete = models.CASCADE)
+    creado = models.DateTimeField(auto_now_add=True)  
+    
+    def __str__(self):
+        
+        return f'Pedido #{self.id} - {self.user_id.username}' 
+    class Meta:
+        db_table = 'Pedidos'
+        verbose_name = 'pedido'
+        verbose_name_plural = 'pedidos'
+        ordering = ['id']
 
-# Create your models here.
+class LineaPedido(models.Model):
+    user_id = models.ForeignKey(User, on_delete = models.CASCADE)
+    producto_id = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    pedido_id = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    cantidad = models.PositiveIntegerField(default=1)
+    creado = models.DateTimeField(auto_now_add=True)  
+    
+    def __str__(self):
+        
+        return f'Linea de pedido#{self.id} - {self.cantidad} unidad/s de {self.producto_id.nombre}'
+    class Meta:
+        db_table = 'LineaPedidos'
+        verbose_name = 'LineaPedido'
+        verbose_name_plural = 'LineaPedidos'
+        ordering = ['id']
