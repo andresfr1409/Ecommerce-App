5041df38e7869751f866c64ed2c411bdaf761f36
Andres.14 <feliperin14@hotmail.es>
2024-01-17 19:44:34 -0500

    creacion de view para procesar la compra de productos del carrito

--- Pedidos/views.py
+++ Pedidos/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from CarroCompras.carro import Carro
+from Pedidos.models import Pedido,LineaPedido
 
-# Create your views here.
+@login_required
+def procesar_compra(request):
+    
+    pedido = Pedido.objects.create(user = request.user)
+    carro = Carro(request)
+    lineas_pedido = list()
+    for key,value in carro.carro.items():
+        lineas_pedido.append(LineaPedido(
+            
+            producto_id = key,
+            cantidad = value["cantidad"],
+            user = request.user,
+            pedido = pedido
+        ))
+    
+    LineaPedido.objects.bulk_create(lineas_pedido)
+    
+    '''
+    enviar_mail(
+        pedido = pedido,
+        lineas_pedido = lineas_pedido,
+        nombre_usuario = request.username,
+        email_usuario = request.usermail,
+    )
+    '''
+    messages.success(request, "Compra realizada exitosamente")
+    
+    return redirect('Tienda')
